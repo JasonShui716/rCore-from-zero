e22b5bbe1375759cbe8dcfc202addf1447c790bb
JasonShui716 <jiashui@hotmail.com>
2023-05-19 00:00:39 +0800

    main
    Signed-off-by: JasonShui716 <jiashui@hotmail.com>

--- hello_world/src/entry.asm
+++ hello_world/src/entry.asm
@@ -2,3 +2,11 @@
      .globl _start
  _start:
     li x1, 100
+    call main
+
+    .section .bss.stack
+    .globl boot_stack_lower_bound
+boot_stack_lower_bound:
+    .space 4096 * 16
+    .globl boot_stack_top
+boot_stack_top:
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -6,3 +6,20 @@ mod console;
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn main() -> ! {
+    clear_bss();
+    loop {}
+}
+#[inline(never)]
+#[no_mangle]
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
